7f0a29432fdae8fc3839263488dcc986fb1bb85d
Diego Augusto <diegofabar@hotmail.com>
2013-09-09 00:40:51 -0300

    Implementação dos métodos para Ler e Escrever Arquivos

--- utilitarios-paa/src/Program.java
+++ utilitarios-paa/src/Program.java
@@ -1,18 +1,124 @@
-/**
- * 
- */
+import java.io.*;
 
 /**
  * @author Diego Augusto de Faria Barros
  * Classe Principal
  */
 public class Program {
+	
+	static File arquivoEntrada;
+	static File arquivoSaida;
+	
+	
+	static void Inicializa() {
+		
+		
+		
+	} // Fim do método Inicializa
+	
+	
+	/**
+	 * @param arquivo Arquivo de entrada
+	 * @return Uma string com o conteúdo do arquivo
+	 */
+	static String LerArquivo(File arquivo) { 
+		
+		StringBuilder conteudo = new StringBuilder();
+		
+		try {
+			
+			BufferedReader leitor = new BufferedReader(new FileReader(arquivo));
+			String linha = leitor.readLine();
+			
+			while (linha != null) {
+				conteudo.append(linha);
+				conteudo.append("\n");
+			}
+			
+			leitor.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return conteudo.toString();
+		
+	} // Fim do método LerArquivo
+	
+	
+	/**
+	 * Grava um novo arquivo no diretório do sistema
+	 * @param arquivo Diretório onde o arquivo será gravado
+	 * @param conteudo Conteúdo textual do arquivo
+	 */
+	static void SalvarArquivo(File arquivo, String conteudo) {
+		
+		try {
+			BufferedWriter escritor = new BufferedWriter(new FileWriter(arquivo));
+			escritor.write(conteudo);
+			escritor.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	} // Fim do método SalvarArquivo
+	
+	/**
+	 * Grava um novo arquivo no diretório do sistema
+	 * @param arquivo Diretório onde o arquivo será gravado
+	 * @param conteudo Conteúdo textual do arquivo
+	 * @param acrescentarConteudo true para que o conteúdo ser anexado ao arquivo
+	 */
+	static void SalvarArquivo(File arquivo, String conteudo, boolean acrescentarConteudo) {
+		
+		try {
+			
+			BufferedWriter escritor;
+			
+			if(acrescentarConteudo)
+				escritor = new BufferedWriter(new FileWriter(arquivo,true));
+			else
+				escritor = new BufferedWriter(new FileWriter(arquivo));
+			
+			escritor.write(conteudo);
+			escritor.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	} // Fim do método SalvarArquivo
+	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		
+		
+	
 
 	} // Fim do método Main
 
 } // Fim da classe Program
+
+
+/*
+ ************************************************************************
+ *            	Universidade Federal de Minas Gerais	     	        *
+ *         Programa de Pós-Graduação em Ciência da Computação           *
+ *                Mestrado em Ciência da Computação                     *
+ *                                                                      *								
+ *  Trabalho: 															*                                                                      
+ * 	Disciplina: Projeto e Análise de Algoritmos                         *
+ * Professores: Luiz Chaimowicz, Wagner Meira, 							*
+ * 				Gisele Pappa, Jussara Almeida                           *
+ *    Trabalho:  														*
+ * 	      Nome: Diego Augusto de Faria Barros                           *
+ *   																	*
+ *                                                                      *
+ *              Belo Horizonte, XX de mmmmmmmmm de 2013  	            *
+ ************************************************************************
+*/
